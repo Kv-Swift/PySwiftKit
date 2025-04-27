aa0e888a7d7cba67f791ed84fae485654cd80002
kivyswiftlink <pythonswiftlink@gmail.com>
2025-04-27 09:18:01 +0000

    Update from https://github.com/PythonSwiftLink/PySwiftKit/commit/77bd8e38db8ff109aa5e889c8d7274ebe7a04764

--- Sources/PyCallable/PyCallable.swift
+++ Sources/PyCallable/PyCallable.swift
@@ -61,7 +61,6 @@ public extension PyMethodDef {
     }
 }
 
-
 public extension PyMethodDef {
     
     init(ml_name: String, ml_flags: Int32, ml_doc: String? = nil,  ml_meth: PySwiftFunctionFast) {
@@ -91,6 +90,15 @@ public extension PyMethodDef {
         )
     }
     
+    init(ml_name: String, ml_flags: Int32, ml_doc: String? = nil,  ml_meth: PySwiftFunction) {
+        self.init(
+            ml_name: cString(ml_name),
+            ml_meth: unsafeBitCast(ml_meth, to: PyCFunction.self),
+            ml_flags: ml_flags,
+            ml_doc: handleDocString(ml_doc)
+        )
+    }
+    
     init(ml_name: String, ml_flags: Int32, ml_doc: String? = nil,  ml_meth: _PyCFunctionFast) {
         self.init(
             ml_name: cString(ml_name),
@@ -107,5 +115,18 @@ public extension PyMethodDef {
             ml_flags: ml_flags,
             ml_doc: handleDocString(ml_doc)
         )
+    }
+}
+
+@resultBuilder
+public struct PyMethodBuilder {
+    public static func buildBlock(_ components: PyMethodDef...) -> [PyMethodDef] {
+        components
+    }
+}
+
+extension Array where Element == PyMethodDef {
+    public init(@PyMethodBuilder methods: () -> [PyMethodDef]) {
+        self = methods()
     }
 }
